2c261e184f2dd3b4769ab15f6cdbbda21d4f86a2
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-24 16:35:56 -0400

    Set up redux toolkit to include required params for fetch api calls

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
+/*const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '84c143d4f2msh3dc3a176c01524ep11d3c9jsnf1dacb849802',
 		'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
 	}
-};
+};*/
 
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+/*fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.catch(err => console.error(err));*/
 
   export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+      baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+      prepareHeaders: (headers) => {
+        headers.set('X-RapidAPI-Key', '84c143d4f2msh3dc3a176c01524ep11d3c9jsnf1dacb849802');
+
+        return headers;
+      }
+    })
   });
