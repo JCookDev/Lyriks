cbbea482b955f566d8f5d86c8c67229724518bc5
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-24 16:21:36 -0400

    Use redux toolkit to create api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '84c143d4f2msh3dc3a176c01524ep11d3c9jsnf1dacb849802',
+		'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+	}
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+  export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+  });
