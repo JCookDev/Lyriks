663b01899aba398779488f373fb8adbf2dd7b379
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-25 16:02:21 -0400

    Make Top Charts section responsive, add labels and set up nav links to display more artists

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -33,7 +33,15 @@ const TopPlay = () => {
   };
 
   return (
-    <div ref={divRef}>
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 };
