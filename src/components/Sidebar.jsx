4b3bc6bc6cb03cb6fecaac5f5d0d36dd0c9c8485
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-25 14:44:11 -0400

    Add menu icon for mobile devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiOutlineMenu } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
 import { logo } from '../assets';
@@ -29,6 +30,12 @@ const Sidebar = () => {
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
+      </div>
+      
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" />}
       </div>
     </>
   )
