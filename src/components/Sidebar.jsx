0864e9409d5b3d681dca422fab0019d70af532ff
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-25 14:14:34 -0400

    Make sidebar coponent usable for desktop and render navbar links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,16 @@ import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
+const NavLinks = () => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink>
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -12,6 +22,7 @@ const Sidebar = () => {
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
       </div>
     </>
   )
