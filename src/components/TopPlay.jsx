32bf5e435771fde0bdacd8f7a19e4917422e3019
Jeffrey Cook <cookturingedu@gmail.com>
2022-10-26 14:43:35 -0400

    Make minor changes to update screen-size

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,7 +11,7 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
@@ -25,8 +25,15 @@ const TopChartCard = ({ song, i }) => (
         </Link>
       </div>
     </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
   </div>
-)
+);
 
 const TopPlay = () => {
   const dispatch = useDispatch();
@@ -44,13 +51,13 @@ const TopPlay = () => {
     dispatch(playPause(false))
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
 
   return (
-    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1:max-w-[500px] max-w-full flex flex-col">
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Charts</h2>
@@ -60,12 +67,16 @@ const TopPlay = () => {
         </div>
 
         <div className="mt-4 flex flex-col gap-1">
-          {topPlays?.map((song, i) =>(
+          {topPlays?.map((song, i) => (
             <TopChartCard
               key={song.key}
               song={song}
               i={i}
-           />
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
           ))}
         </div>
       </div>
@@ -77,6 +88,7 @@ const TopPlay = () => {
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
+
         <Swiper
           slidesPerView="auto"
           spaceBetween={15}
@@ -93,15 +105,14 @@ const TopPlay = () => {
               className="shadow-lg rounded-full animate-slideright"
             >
               <Link to={`/artists/${song?.artists[0].adamid}`}>
-                <img src={song?.images.background} alt="name"
-                className="rounded-full w-full object-cover" />
+                <img src={song?.images.background} alt="name" className="rounded-full w-full object-cover" />
               </Link>
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
     </div>
-  )
+  );
 };
 
 export default TopPlay;
